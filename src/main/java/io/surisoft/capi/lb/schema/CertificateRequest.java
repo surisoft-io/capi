e4e18785cb270c3012d168b48fce97f200ce6289
rodrigoserracoelho <rodrigoserracoelho@gmail.com>
2022-05-05 17:32:39 +0200

    Schema Refactor

--- src/main/java/io/surisoft/capi/lb/schema/CertificateRequest.java
+++ src/main/java/io/surisoft/capi/lb/schema/CertificateRequest.java
@@ -1,14 +1,36 @@
 package io.surisoft.capi.lb.schema;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Data;
 
 import java.io.Serializable;
 
-@Data
 @Schema(hidden = true)
 public class CertificateRequest implements Serializable {
     private String url;
     private int port = 443;
     private String alias;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
 }
