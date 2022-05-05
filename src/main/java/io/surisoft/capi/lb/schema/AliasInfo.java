3c2dc28679cf42a801d5094de4eb2d7655161650
rodrigoserracoelho <rodrigoserracoelho@gmail.com>
2022-05-05 17:34:24 +0200

    Schema Refactor

--- src/main/java/io/surisoft/capi/lb/schema/AliasInfo.java
+++ src/main/java/io/surisoft/capi/lb/schema/AliasInfo.java
@@ -1,13 +1,44 @@
 package io.surisoft.capi.lb.schema;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Data;
 
-@Data
 @Schema(hidden = true)
 public class AliasInfo {
+
     private String alias;
     private String issuerDN;
     private String subjectDN;
     private String additionalInfo;
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getIssuerDN() {
+        return issuerDN;
+    }
+
+    public void setIssuerDN(String issuerDN) {
+        this.issuerDN = issuerDN;
+    }
+
+    public String getSubjectDN() {
+        return subjectDN;
+    }
+
+    public void setSubjectDN(String subjectDN) {
+        this.subjectDN = subjectDN;
+    }
+
+    public String getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public void setAdditionalInfo(String additionalInfo) {
+        this.additionalInfo = additionalInfo;
+    }
 }
