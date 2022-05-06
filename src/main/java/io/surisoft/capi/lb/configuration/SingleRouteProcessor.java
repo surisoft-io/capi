76752436fd9fabcffe038cbca3bb7971ed6b8ef8
rodrigoserracoelho <rodrigoserracoelho@gmail.com>
2022-05-06 21:08:37 +0200

    Camel Update 16

--- src/main/java/io/surisoft/capi/lb/configuration/SingleRouteProcessor.java
+++ src/main/java/io/surisoft/capi/lb/configuration/SingleRouteProcessor.java
@@ -217,12 +217,8 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.RouteDefinition;
 import org.apache.camel.model.rest.RestDefinition;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SingleRouteProcessor extends RouteBuilder {
-
-    private static final Logger log = LoggerFactory.getLogger(SingleRouteProcessor.class);
 
     private RouteUtils routeUtils;
     private MetricsProcessor metricsProcessor;
