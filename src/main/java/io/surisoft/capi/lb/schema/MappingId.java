054567a4ef4ef4a6c9164c05cfc623117ebd6a6e
Rodrigo Serra Coelho <rodrigoserracoelho@gmail.com>
2023-06-16 22:56:38 +0200

    Unit test refactoring

--- src/main/java/io/surisoft/capi/lb/schema/MappingId.java
+++ src/main/java/io/surisoft/capi/lb/schema/MappingId.java
@@ -6,4 +6,28 @@ public class MappingId implements Serializable {
     private String rootContext;
     private String hostname;
     private int port;
+
+    public String getRootContext() {
+        return rootContext;
+    }
+
+    public void setRootContext(String rootContext) {
+        this.rootContext = rootContext;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
 }
