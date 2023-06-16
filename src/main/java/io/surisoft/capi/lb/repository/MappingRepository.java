64a0fb15f7e47ea68196ab2910050e614abd2b1d
Rodrigo Serra Coelho <rodrigoserracoelho@gmail.com>
2023-06-16 23:13:08 +0200

    Unit test refactoring

--- src/main/java/io/surisoft/capi/lb/repository/MappingRepository.java
+++ src/main/java/io/surisoft/capi/lb/repository/MappingRepository.java
@@ -17,14 +17,6 @@ public class MappingRepository {
     @Autowired(required = false)
     private EntityManager entityManager;
 
-    public Optional<Mapping> findByRootContextAndHostnameAndPort(String rootContext, String hostname, int port) {
-        TypedQuery<Mapping> query = entityManager.createQuery("SELECT m FROM Mapping m WHERE m.rootContext = :rootContext and m.hostname = :hostname and port = :port" , Mapping.class);
-        query.setParameter("rootContext", rootContext);
-        query.setParameter("hostname", hostname);
-        query.setParameter("port", port);
-        return Optional.ofNullable(query.getSingleResult());
-    }
-
     public void delete(Mapping mapping) {
         Mapping managed = entityManager.merge(mapping);
         entityManager.remove(managed);
