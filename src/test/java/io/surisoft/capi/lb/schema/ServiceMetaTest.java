3f03e6ea691c85bdffe6147b8932683170646aa9
Rodrigo Serra Coelho <rodrigoserracoelho@gmail.com>
2023-06-16 22:42:54 +0200

    Unit test refactoring

---
+++ src/test/java/io/surisoft/capi/lb/schema/ServiceMetaTest.java
@@ -0,0 +1,63 @@
+package io.surisoft.capi.lb.schema;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceMetaTest {
+
+    private ServiceMeta serviceMeta;
+    @BeforeEach
+    void setUp() {
+        serviceMeta = new ServiceMeta();
+    }
+
+    @Test
+    void testSecured() {
+        serviceMeta.setSecured(true);
+        assertTrue(serviceMeta.isSecured());
+    }
+
+    @Test
+    void testRootContext() {
+        serviceMeta.setRootContext("/");
+        assertEquals("/", serviceMeta.getRootContext());
+    }
+
+    @Test
+    void testSchema() {
+        serviceMeta.setSchema("http");
+        assertEquals("http", serviceMeta.getSchema());
+    }
+
+    @Test
+    void testTenantAware() {
+        serviceMeta.setTenantAware(true);
+        assertTrue(serviceMeta.isTenantAware());
+    }
+
+    @Test
+    void testTenantId() {
+        serviceMeta.setTenantId("ID");
+        assertEquals("ID", serviceMeta.getTenantId());
+    }
+
+    @Test
+    void testGroup() {
+        serviceMeta.setGroup("dev");
+        assertEquals("dev", serviceMeta.getGroup());
+    }
+
+    @Test
+    void testB3TraceId() {
+        serviceMeta.setB3TraceId(true);
+        assertTrue(serviceMeta.isB3TraceId());
+    }
+
+    @Test
+    void testIngress() {
+        serviceMeta.setIngress("ingress");
+        assertEquals("ingress", serviceMeta.getIngress());
+    }
+}
