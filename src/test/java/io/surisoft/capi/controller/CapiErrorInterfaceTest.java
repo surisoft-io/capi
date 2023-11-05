95e8cb6daac374c838771a7037eaf3f19c124d18
Rodrigo Serra Coelho <rodrigoserracoelho@gmail.com>
2023-11-05 16:12:37 +0100

    Error Interface Unit Testing

---
+++ src/test/java/io/surisoft/capi/controller/CapiErrorInterfaceTest.java
@@ -0,0 +1,100 @@
+package io.surisoft.capi.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class CapiErrorInterfaceTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void initialize() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    void testHandleGet() throws Exception {
+        // Setup
+        // Run the test
+        final MockHttpServletResponse response = mockMvc.perform(get("/error")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        // Verify the results
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        assertThat(response.getContentAsString()).isEqualTo("{\"errorMessage\":\"The requested route is not available, please try again later on.\",\"errorCode\":\"org.springframework.http.HttpStatus.BAD_REQUEST\"}");
+    }
+
+    @Test
+    void testHandlePost() throws Exception {
+        // Setup
+        // Run the test
+        final MockHttpServletResponse response = mockMvc.perform(post("/error")
+                        .content("content").contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        // Verify the results
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        assertThat(response.getContentAsString()).isEqualTo("{\"errorMessage\":\"The requested route is not available, please try again later on.\",\"errorCode\":\"org.springframework.http.HttpStatus.BAD_REQUEST\"}");
+    }
+
+    @Test
+    void testHandlePut() throws Exception {
+        // Setup
+        // Run the test
+        final MockHttpServletResponse response = mockMvc.perform(put("/error")
+                        .content("content").contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        // Verify the results
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        assertThat(response.getContentAsString()).isEqualTo("{\"errorMessage\":\"The requested route is not available, please try again later on.\",\"errorCode\":\"org.springframework.http.HttpStatus.BAD_REQUEST\"}");
+    }
+
+    @Test
+    void testHandleDelete() throws Exception {
+        // Setup
+        // Run the test
+        final MockHttpServletResponse response = mockMvc.perform(delete("/error")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        // Verify the results
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        assertThat(response.getContentAsString()).isEqualTo("{\"errorMessage\":\"The requested route is not available, please try again later on.\",\"errorCode\":\"org.springframework.http.HttpStatus.BAD_REQUEST\"}");
+    }
+
+    @Test
+    void testHandlePatch() throws Exception {
+        // Setup
+        // Run the test
+        final MockHttpServletResponse response = mockMvc.perform(patch("/error")
+                        .content("content").contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        // Verify the results
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        assertThat(response.getContentAsString()).isEqualTo("{\"errorMessage\":\"The requested route is not available, please try again later on.\",\"errorCode\":\"org.springframework.http.HttpStatus.BAD_REQUEST\"}");
+    }
+}
